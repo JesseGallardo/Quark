19a3f31239dc619507ccf9f972168c03d632a401
Andrew Davis <drew@whosdrew.com>
2018-04-27 17:34:39 -0400

    Remove old shell script

--- Atom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Atom Portable Launcher
-# Created by Andrew Davis (github.com/andrewsdavis)
-# Updated April 27, 2018
-
-echo Opening Atom... # Verbose program start
-
-export ATOM_HOME="$PWD/Atom/.atom" # Set .atom directory to current directory's .atom folder
-$PWD/Atom/Atom-Linux/usr/share/atom/atom # Run atom from current directory's copy
