b01b28d584edf0fd33b4d8fb3b9ce4e0cab6f7b0
Andrew Davis <drew@andrewsdavis.me>
2018-06-30 13:58:11 -0400

    Create Linux launcher in C++

---
+++ src/Quark-Linux.cpp
@@ -0,0 +1,14 @@
+/*
+ * Quark (Atom-Portable) Installer v0.7
+ * Created by Andrew Davis (github.com/andrewsdavis)
+ * Licensed under MIT License
+ *
+ * Updated June 14, 2018
+ */
+
+int main(int argc, char **argv) {
+
+    // TODO: Check for updates
+
+    system("Atom/Atom-Linux/atom --portable"); // Launch Atom in portable mode
+}
